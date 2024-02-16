6ecd63cbc3e813fd6b7aaf2e50d9d38ad4b2955f
Selim Şahin <selimsahin.sns@gmail.com>
2024-02-16 16:11:00 +0300

    initial commit

---
+++ src/main/java/com/selimsahin/jobseekr/JobseekrApplication.java
@@ -0,0 +1,13 @@
+package com.selimsahin.jobseekr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobseekrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobseekrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selimsahin/jobseekr/JobseekrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selimsahin.jobseekr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobseekrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
